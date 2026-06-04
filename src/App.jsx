b43e81ebb45cd75cb3a7dabd763cc07eb1e3c692
Abhijeet Soni <abhisonijeet123@gmail.com>
2026-06-04 12:51:34 +0530

    term file added to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,22 @@ import Login from "./pages/Login";
 import Chat from "./pages/Chat";
 import HowToUsePage from "./pages/HowToUsePage";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
+import TermsOfService from "./pages/TermsOfService";
 
 export default function App() {
   return (
     <Routes>
       {/* Landing */}
       <Route path="/" element={<LandingPage />} />
-
       {/* Auth */}
       <Route path="/login" element={<Login />} />
-
       {/* App */}
       <Route path="/chat" element={<Chat />} />
-
       {/* Docs */}
       <Route path="/how-to-use" element={<HowToUsePage />} />
       <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+  
+      <Route path="/terms" element={<TermsOfService />} />
     </Routes>
   );
 }
